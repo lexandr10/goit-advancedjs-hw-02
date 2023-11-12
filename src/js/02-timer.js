d9e54988500fc4ab21450d492f4061dccf2b1f58
lexandr10 <alexandrenko3006@gmail.com>
2023-11-12 16:55:08 -0500

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
+import 'flatpickr/dist/flatpickr.min.css';
 let timeDifference = 0;
 let timerId = null;
 let formatDate = null;
@@ -14,16 +12,36 @@ const elements = {
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
+
+elements.btnStart.setAttribute('disabled', true);
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    currentDifferenceDate(selectedDates[0]);
     console.log(selectedDates[0]);
+    pickDate(selectedDates[0]);
   },
 };
+flatpickr(elements.inputPickDate, options);
+
+function pickDate(selectedDates) {
+  const current = Date.now();
+  if (selectedDates < current) {
+    return alert('Please choose a date in the future');
+  }
+  timeDifference = selectedDates.getTime() - current;
+  formatDate = convertMs(timeDifference);
+  renderDate(formatDate);
+  elements.btnStart.removeAttribute('disabled', true);
+}
+elements.btnStart.addEventListener('click', handlerStart);
+
+function handlerStart(event) {
+  timerId = setInterval(startTime, 1000);
+}
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -42,35 +60,11 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-flatpickr(elements.inputPickDate, options);
-elements.btnStart.setAttribute('disabled', 'disabled');
 
-elements.btnStart.addEventListener('click', handlerOnTime);
-
-function handlerOnTime() {
-  timerId = setInterval(startTimer, 1000);
-}
-
-window.addEventListener('keydown', e => {
-  if (e.code === 'Escape' && timerId) {
-    clearInterval(timerId);
-
-    elements.inputPickDate.removeAttribute('disabled');
-    elements.btnStart.setAttribute('disabled', true);
-
-    elements.seconds.textContent = '00';
-    elements.minutes.textContent = '00';
-    elements.hours.textContent = '00';
-    elements.days.textContent = '00';
-  }
-});
-
-function startTimer() {
+function startTime() {
   elements.btnStart.setAttribute('disabled', true);
   elements.inputPickDate.setAttribute('disabled', true);
-
   timeDifference -= 1000;
-
   if (elements.seconds.textContent <= 0 && elements.minutes.textContent <= 0) {
     alert('Time end');
     clearInterval(timerId);
@@ -79,24 +73,23 @@ function startTimer() {
     renderDate(formatDate);
   }
 }
+
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape' && timerId) {
+    clearInterval(timerId);
+
+    imputDatePickerRef.removeAttribute('disabled');
+    btnStartRef.setAttribute('disabled', true);
+
+    secondsRef.textContent = '00';
+    minutesRef.textContent = '00';
+    hoursRef.textContent = '00';
+    daysRef.textContent = '00';
+  }
+});
 function renderDate(formatDate) {
   elements.seconds.textContent = formatDate.seconds;
   elements.minutes.textContent = formatDate.minutes;
   elements.hours.textContent = formatDate.hours;
   elements.days.textContent = formatDate.days;
-}
-
-function currentDifferenceDate(selectedDates) {
-  const currentDate = Date.now();
-
-  if (selectedDates < currentDate) {
-    elements.btnStart.setAttribute('disabled', true);
-    return alert('Please choose a date in the future');
-  }
-
-  timeDifference = selectedDates.getTime() - currentDate;
-  formatDate = convertMs(timeDifference);
-
-  renderDate(formatDate);
-  elements.btnStart.removeAttribute('disabled');
 }
